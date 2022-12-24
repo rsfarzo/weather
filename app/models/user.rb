caba93bc4c6cbd5c7284914820133f667d39c689
Robert Sfarzo <rhsfarzo@gmail.com>
2022-12-24 01:28:56 -0600

    Make a basic User model (including secure passwords)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
     before_save { self.email = email.downcase }
-    validates :name, presence: true, length: { maximum: 50 }
+    validates :name, presence: true, length: { minimum:9, maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, length: { maximum: 25 },
+    validates :email, presence: true, length: { minimum:10, maximum: 25 },
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
+    has_secure_password  # provides:
+    # The ability to save a securely hashed password_digest attribute to the database 
+    # A pair of virtual attributes21 (password and password_confirmation), including presence validations upon object creation and a validation requiring that they match
+    # An authenticate method that returns the user when the password is correct (and false otherwise)
+    validates :password, presence: true, length: { minimum: 6,maximum: 25 }
 end
