445a74483d19767f8660db1baba276a77e111082
Robert Sfarzo <rhsfarzo@gmail.com>
2022-12-23 22:03:54 -0600

    sign up

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -34,6 +34,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get signup" do
     get signup_path
     assert_response :success
-    #assert_select "title", "Sign Up | #{@base_title}"
+    assert_select "title", "Sign Up | #{@base_title}"
   end
 end
