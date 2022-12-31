c9758d588763dc1ac7e60b6bc7a061aa9e134791
Robert Sfarzo <rhsfarzo@gmail.com>
2022-12-30 20:33:50 -0600

    importmap:install turbo:install stimulus:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'static_pages#home' #"application#hello" leads to the creation of a Rails helper called root_url
   #get '/home',    to:'static_pages#home'
   get '/help',    to:'static_pages#help'
   get '/about',    to:'static_pages#about'
   get '/contact',    to:'static_pages#contact'
   get '/signup',  to: 'users#new'
+  get '/login',   to: 'sessions#new'
+  post '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  #get '/logout',  to: 'sessions#destroy'
+
   resources :users
 end
