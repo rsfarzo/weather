bfb9216f89c0826c33e81d210b5b066adb92bb5a
Robert Sfarzo <rhsfarzo@gmail.com>
2023-01-26 16:01:08 -0600

    seeded City with geo location

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,19 @@
-# Create a main sample user.
+# HOW TO SEPARATE SEEDS INTO SEPARATE FILES?
 
+=begin
+require 'csv'
+CSV.foreach(Rails.root.join('lib/uscities.csv'), headers: true) do |row|
+  
+  City.create({
+    name: row[0],
+    state: row[1],
+    lat: row[2],
+    lng: row[3]
+  })
+end
+=end
 # Generate a bunch of additional users.
+=begin
 99.times do |n|
   name  = Faker::Name.name
   email = "user-#{n+1}@seed.org"
@@ -10,3 +23,4 @@
                password:              password,
                password_confirmation: password)
 end
+=end
