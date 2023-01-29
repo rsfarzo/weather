86f1c457b1069495667f75516c42113a721c383f
Robert Sfarzo <rhsfarzo@gmail.com>
2023-01-29 09:52:16 -0600

    limit to 50 city's

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,17 @@ ct=0
 CSV.foreach(Rails.root.join('lib/uscities.csv'), headers: true) do |row|
   ct=ct+1
   if ct<51
-  City.create({
-    name: row[0],
-    state: row[1],
-    lat: row[2],
-    lng: row[3]
-  })
-end
+    City.create({
+      name: row[0],
+      state: row[1],
+      lat: row[2],
+      lng: row[3]
+    })
+  end
 end
 #=end
 # Generate a bunch of additional users.
-#=begin
+=begin
 25.times do |n|
   name  = Faker::Name.name
   email = "user-#{n+1}@seed.org"
@@ -28,3 +28,4 @@ end
 end
 #=end
 User.create(:name=>"Robert Admin",:email=>"robert@admin.org",:password=>"password",:password_confirmation=>"password",:admin=>true)
+=end
